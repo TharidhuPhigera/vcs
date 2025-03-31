7b97843dc2b32c4f9b1dca3e805daffc43cdfffb
Tharidhu <tharidhuphigera@gmail.com>
2025-03-31 17:08:08 +0200

    fix: nextjs

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
   trailingSlash: true,
   images: {
     domains: ['res.cloudinary.com'],
